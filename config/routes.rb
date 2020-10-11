1707b9cc7a3af6fe2f1fae560309f1235a49eb91
vagrant <vagrant@localhost.localdomain>
2020-10-11 10:58:22 +0000

    ヘッダーとフッダー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  root 'products#index'
+
+  resources :users, only: [:show, :edit, :update]
+
+  namespace :products do
+    resources :searches, only: [:index,:show]
+  end
+  
+  resources :categories, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20201010093240_create_products.rb
@@ -0,0 +1,20 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name, limit: 191,null:false,index: true
+      t.integer :price, index: true
+      t.text :explain,null:false
+      t.integer :postage,null:false
+      t.integer :status
+      t.integer :shipping_date
+      t.integer :size
+      t.integer :brand_id
+      t.integer :category_id
+      t.integer :prefecture
+      t.integer :buyer_id
+      t.references :user,index: true, foreign_key: true
+      t.string :reservation_email
+      t.timestamps
+    end
+  end
+end
